6bf132f56275b12067e235362d314ee8a7c12678
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-03-06 16:31:30 +0530

    Firebase connected and sign up overflow fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/Home.dart';
 import 'package:flutter/material.dart';
 import 'screens/Home.dart';
@@ -5,15 +6,18 @@ import 'screens/Search.dart';
 import 'screens/sign up.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main()
+void main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MaterialApp(
     home: Home(),
-    routes:
-    {
-      "/" :(context)=> Home(),
-      "/signup":(context)=>SignUp(),
-    },
+    // routes:
+    // {
+    //   "/" :(context)=> Home(),
+    //   "/signup":(context)=>SignUp(),
+    // },
     debugShowCheckedModeBanner: false,
   ));
 }
