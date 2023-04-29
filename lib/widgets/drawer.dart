988c4289203dda7f911dd374308a45c971e58ab7
aanchal193 <aanchals2003@gmail.com>
2023-04-29 17:10:12 +0530

    edit profile added

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -72,7 +72,7 @@ Widget returndrawer(Usermap x,BuildContext context){
               ListTile(
                 onTap:()
                 {
-                  Navigator.pushNamed(context,'/editProfile');
+                  Navigator.pushNamed(context,'/editprofile');
 
                 },
 
